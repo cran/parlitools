00c3b267a0bb732c4941a78b357f00a8937bc1f9
Evan Odell <evanodell91@gmail.com>
2019-05-30 13:00:03 +0000

    version 0.3.3

--- tests/testthat/test-council_seats.R
+++ tests/testthat/test-council_seats.R
@@ -1,9 +1,11 @@
 context("test-council_seats")
 
 test_that("council_seats works", {
+  skip_on_cran()
+  
   summary <- council_seats(councillors = FALSE)
   expect_true(tibble::is_tibble(summary))
-  expect_true(nrow(summary)==423)
-  
-  
+  expect_length(summary, 18)
+
+
 })
